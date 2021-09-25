91660448be78a8adf8ef8a0e027163128ba212c0
Edwin Guevara <admin@pixelweb.com.co>
2021-09-25 12:24:01 -0500

    regf

--- src/components/registerwizard/wizardAnimated.jsx
+++ src/components/registerwizard/wizardAnimated.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
-
-
+import closebt from '../../assets/img/close.png'
+import backbt from '../../assets/img/back.png'
+import nextbt from '../../assets/img/next.png'
 const Wizard = props => {
 
 
@@ -57,21 +58,21 @@ const Wizard = props => {
 
   return (
     
-   <div class="">
-  <div class="row">
-    <div class="col-sm-12 col-md-12 col-md-offset-4 col-lg-12 col-lg-offset-3">
+   <div className="container-fluid">
+  <div className="row">
+    <div className="col-sm-12 col-md-12 col-md-offset-4 col-lg-12 col-lg-offset-3">
       {/* <!-- Offset --> */}
-      <div class="form-step-wrap">
-      <div id="toph" class="row">
-          <div className="col-md-2 col-sm-2 col-xs-2">X</div>
+      <div className="form-step-wrap">
+      <div id="toph" className="row">
+          <div className="col-md-2 col-sm-2 col-xs-2"><img class="img-responsive closeb-t" src={closebt}/></div>
           <div className="col-md-8 col-sm-8 col-xs-8"></div>
           <div className="col-md-2 col-sm-2 col-xs-2" align="center"><h3>1</h3></div>
       </div>
-          <div id="step1box" class="slider-step first-step step" data-next-step="step-mortgage-balance">
-            <div class="row tall step1">
+          <div id="step1box" className="slider-step first-step step" data-next-step="step-mortgage-balance">
+            <div className="row tall step1">
 
-              <div class="col-xs-12 form-questions">
-                <div class="row">
+              <div className="col-xs-12 form-questions">
+                <div className="row">
                 <p className="text-step" >Estamos contentos que quieras hacer parte de nuestra familia Neggo y por ello, queremos conocerte mejor,
                   a continuacion te vamos hacer algunas preguntas para que podamos reconocerte en nuestra plataforma...
                  </p>
@@ -83,17 +84,12 @@ const Wizard = props => {
               {/* <!-- /Form Questions --> */}
             </div>
             {/* <!-- /Row --> */}
-            <div class="row">
-
-              <div class="col-xs-12 border">
-                <input type='button' value='Continue' tabindex="3" class="btn-next btn-success form-control" />
-              </div>
-            </div>
+            
           </div>
           {/* <!-- Upper Text --> */}
-          <div id="step-mortgage-balance" class="slider-step step sig" data-next-step="step-home-value" data-back-to="step1box">
-            <div class="row step2">
-              <div class="col-xs-12  ">
+          <div id="step-mortgage-balance" className="slider-step step sig" data-next-step="step-home-value" data-back-to="step1box">
+            <div className="row step2">
+              <div className="col-xs-12  ">
               <h5 style={{color: 'white'}} >Cuales son tus nombres?</h5>
                 <input
                 className="form-control"
@@ -102,22 +98,13 @@ const Wizard = props => {
                 />
               </div>
             </div>
-            <div class="row">
-              <div class="col-xs-12">
-                <input type='button' value='Continue' tabindex="5" class="btn-success form-control btn-next" />
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-xs-12">
-                <a class="btn-back">back</a>
-              </div>
-            </div>
+            
           </div>
 
-          <div id="step-home-value" class="slider-step step sig" data-next-step="step-email-rate" data-back-to="step-mortgage-balance">
+          <div id="step-home-value" className="slider-step step sig" data-next-step="step-email-rate" data-back-to="step-mortgage-balance">
 
-            <div class="row step3">
-              <div class="col-xs-12  ">
+            <div className="row step3">
+              <div className="col-xs-12  ">
               <h5 style={{color: 'white'}}>Cuales son tus apellidos?</h5>
                 <input
                 className="form-control"
@@ -126,22 +113,13 @@ const Wizard = props => {
                 />
               </div>
             </div>
-            <div class="row">
-              <div class="col-xs-12">
-                <input id="next-home-value" type='button' value='Continue' tabindex="7" class="btn-success form-control btn-next" />
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-xs-12">
-                <a id="back-home-value" class="btn-back">back</a>
-              </div>
-            </div>
+            
           </div>
     
-          <div id="step-email-rate" class="slider-step step sig" data-next-step="step-telefono-buy-process" data-back-to="step-home-value">
+          <div id="step-email-rate" className="slider-step step sig" data-next-step="step-telefono-buy-process" data-back-to="step-home-value">
           
-            <div class="row step4">
-              <div class="col-xs-12  ">
+            <div className="row step4">
+              <div className="col-xs-12  ">
               <h5 style={{color: 'white'}}>Cual es tu correo electrónico?</h5>
                 <input
                 className="form-control"
@@ -150,22 +128,13 @@ const Wizard = props => {
                 />
               </div>
             </div>
-            <div class="row">
-              <div class="col-xs-12">
-                <input id="next-interest-rate" type='button' value='Continue' tabindex="9" class="btn-success form-control btn-next" />
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-xs-12">
-                <a id="back-interest-rate" class="btn-back">back</a>
-              </div>
-            </div>
+           
           </div>
          
-          <div id="step-telefono-buy-process" class="slider-step step sig" data-next-step="step-password-process" data-back-to="step-email-rate">
+          <div id="step-telefono-buy-process" className="slider-step step sig" data-next-step="step-password-process" data-back-to="step-email-rate">
          
-          <div class="row step5">
-            <div class="col-xs-12 ">
+          <div className="row step5">
+            <div className="col-xs-12 ">
             <h5 style={{color: 'white'}}>Un numero de contacto?</h5>
             <p>Ten en cuenta que este será tu usuario.</p>
                 <input
@@ -176,22 +145,22 @@ const Wizard = props => {
 
             </div>
           </div>
-          <div class="row">
-            <div class="col-xs-12">
-              <input id="next-home-buy-process" type='button' value='Continue' tabindex="11" class="btn-success form-control btn-next" />
+          <div className="row">
+            <div className="col-xs-12">
+              <input id="next-home-buy-process" type='button' value='Continue' tabindex="11" className="btn-success form-control btn-next" />
             </div>
           </div>
-          <div class="row">
-            <div class="col-xs-12">
-              <a id="back-home-buy-process" class="btn-back">back</a>
+          <div className="row">
+            <div className="col-xs-12">
+              <a id="back-home-buy-process" className="btn-back">back</a>
             </div>
           </div>
         </div>
 
-        <div id="step-password-process" class="slider-step step sig" data-next-step="step-end-process" data-back-to="step-telefono-buy-process">
+        <div id="step-password-process" className="slider-step step sig" data-next-step="step-end-process" data-back-to="step-telefono-buy-process">
          
-         <div class="row step6">
-           <div class="col-xs-12 ">
+         <div className="row step6">
+           <div className="col-xs-12 ">
            <h5 style={{color: 'white'}}>Escribe tu clave de acceso</h5>
            <p>4 digitos numéricos</p>
                <input
@@ -202,22 +171,13 @@ const Wizard = props => {
 
            </div>
          </div>
-         <div class="row">
-           <div class="col-xs-12">
-             <input id="next-home-buy-process" type='button' value='Continue' tabindex="11" class="btn-success form-control btn-next" />
-           </div>
-         </div>
-         <div class="row">
-           <div class="col-xs-12">
-             <a id="back-home-buy-process" class="btn-back">back</a>
-           </div>
-         </div>
+         
        </div>
 
-       <div id="step-end-process" class="slider-step step last" data-back-to="step-password-process">
+       <div id="step-end-process" className="slider-step step last" data-back-to="step-password-process">
          
-         <div class="row step7">
-           <div class="col-xs-12 ">
+         <div className="row step7">
+           <div className="col-xs-12 ">
            <h3 style={{color: 'white'}}>Genial!!</h3>
            <p className="text-step">
             Para terminar, acepta nuestras condiciones de servicio y privacidad
@@ -238,23 +198,33 @@ const Wizard = props => {
       <p>captcha</p>
            </div>
          </div>
-         <div class="row">
-           <div class="col-xs-12">
-             <input id="next-home-buy-process" type='button' value='Continue' tabindex="15" class="btn-success form-control btn-next" />
-           </div>
-         </div>
-         <div class="row">
-           <div class="col-xs-12">
-             <a id="back-home-buy-process" class="btn-back">back</a>
-           </div>
-         </div>
+        
        </div>
 
-        <div class="clear"></div>
+        <div className="clear"></div>
       </div>
      
     </div>
-  </div></div>
+  </div>
+  
+  <div className="btn-c">
+  <ul className="botones">
+    <li>
+{/*           
+ <input id="next-home-buy-process" type='button' value='Continue' tabindex="11" className="btn-success form-control btn-next" />
+           */}
+
+           <a id="next-home-buy-process" className="btn-back"><img class="" src={backbt}/></a>
+
+         </li>
+         <li>
+           
+             <a id="back-home-buy-process" className=" btn-next"><img class="img-responsive " src={nextbt}/></a>
+           
+         </li>
+  </ul>
+  </div>
+  </div>
   
 
 
